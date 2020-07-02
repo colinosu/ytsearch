dfb34f8b85e797dc600b6545b08560d878843991
colinosu <osullivancj@gmail.com>
2020-07-02 15:36:09 -0400

    Updates

---
+++ precache-manifest.4c7f8131c4f7a45958ca7ea4f46327a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce1d19bf255a6be679f8",
+    "url": "/ytsearch/static/js/runtime~main.ce1d19bf.js"
+  },
+  {
+    "revision": "1662c27401cc5f75697a",
+    "url": "/ytsearch/static/js/main.1662c274.chunk.js"
+  },
+  {
+    "revision": "ba449875abab90ec7bce",
+    "url": "/ytsearch/static/js/1.ba449875.chunk.js"
+  },
+  {
+    "revision": "1662c27401cc5f75697a",
+    "url": "/ytsearch/static/css/main.039a2d48.chunk.css"
+  },
+  {
+    "revision": "d58ca39cb5779dd1bee1ecbc22a4e315",
+    "url": "/ytsearch/index.html"
+  }
+];
